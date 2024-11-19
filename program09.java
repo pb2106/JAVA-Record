30033e59f33211d66e13426340e2acd99df2ec7b
Prabhav M Naik <84378675+pb2106@users.noreply.github.com>
2024-11-20 01:09:31 +0530

    Update and rename program9.java to program09.java

--- program09.java
+++ program09.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class program9 {
+public class program09 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 3 numbers one after another: ");
