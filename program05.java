8b0c4be636c2bb12038cb28b2b83c7f2645381cf
Prabhav M Naik <84378675+pb2106@users.noreply.github.com>
2024-11-20 01:08:30 +0530

    Update and rename program5.java to program05.java

--- program05.java
+++ program05.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class program5 {
+public class program05 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number: ");
